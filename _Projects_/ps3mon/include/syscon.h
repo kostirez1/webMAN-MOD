3b9014c6e04c2554f280e793a0f65bb26495e564
kostirez1 <14951550+kostirez1@users.noreply.github.com>
2022-09-13 21:49:07 +0200

    Syscon error logging improvements

--- _Projects_/ps3mon/include/syscon.h
+++ _Projects_/ps3mon/include/syscon.h
@@ -3,9 +3,12 @@
 #define BEEP3 { system_call_3(SC_RING_BUZZER, 0x1004, 0xa, 0x1b6); }
 
 #define SYSCON_ERROR_CACHE "/dev_hdd0/tmp/ps3mon_syscon_errors.bin"
-#define SYSCON_ERROR_LOG "/dev_hdd0/tmp/ps3_mon_syscon_error_log.txt"
+#define SYSCON_ERROR_LOG "/dev_hdd0/tmp/ps3mon_syscon_error_log.txt"
 #define SYSCON_ERROR_LOG_MAXSIZE _1MB_
 #define SYSCON_ERROR_BUFF_LEN 256
+#define SYSCON_DATE_BUFF_LEN 32
+
+#define PLATFORM_ID_MAX_LEN 6
 
 typedef struct syscon_error {
 	uint32_t error_code;
@@ -15,7 +18,5 @@ typedef struct syscon_error {
 inline int sys_sm_request_error_log(uint8_t offset, uint8_t* unknown0, uint32_t* unknown1, uint32_t* unknown2);
 
 const char * getSysconErrorDesc(uint32_t error_code);
-
-int format_date(char *buf, time_t print_time);
 
 void dump_syscon_errors(void);
--- _Projects_/ps3mon/source/syscon.c
+++ _Projects_/ps3mon/source/syscon.c
@@ -15,6 +15,22 @@ inline int sys_sm_request_error_log(uint8_t offset, uint8_t* unknown0, uint32_t*
 	return_to_user_prog(int);
 }
 
+struct __attribute__((__packed__)) system_info {
+	uint8_t firmware_version_high;
+	uint16_t firmware_version_low;
+	uint8_t reserved;
+	uint8_t unk1[4];
+	char platform_id[8];
+	uint32_t firmware_build;
+	uint8_t unk2[4];
+};
+
+static inline int sys_sm_get_system_info(struct system_info* unknown0)
+{
+	system_call_1(387, (uint64_t)unknown0);
+	return_to_user_prog(int);
+}
+
 const char * getSysconErrorDesc(uint32_t error_code){
 	// Get the last 4 digits
 	switch(error_code & 0xFFFF){
@@ -61,128 +77,293 @@ const char * getSysconErrorDesc(uint32_t error_code){
 	}
 }
 
-int format_date(char *buf, time_t print_time){
-	CellRtcDateTime cDate;
+static const char * getBoardDesc(const char *platform_id)
+{
+	// IDs according to https://www.psdevwiki.com/ps3/Platform_ID
+	// Fat
+	if(!strncmp(platform_id, "Cok14",  PLATFORM_ID_MAX_LEN)) return "COK-001 CECHA/CECHBxx";
+	if(!strncmp(platform_id, "CokB10", PLATFORM_ID_MAX_LEN)) return "COK-002 CECHC/CECHExx";
+	if(!strncmp(platform_id, "CokC12", PLATFORM_ID_MAX_LEN)) return "SEM-001 CECHGxx";
+	if(!strncmp(platform_id, "CokD10", PLATFORM_ID_MAX_LEN)) return "DIA-001 CECHHxx";
+	if(!strncmp(platform_id, "CokE10", PLATFORM_ID_MAX_LEN)) return "DIA-002 CECHKxx";
+	if(!strncmp(platform_id, "Deb01",  PLATFORM_ID_MAX_LEN)) return "DEB-001 DECR-1400";
+	if(!strncmp(platform_id, "CokF10", PLATFORM_ID_MAX_LEN)) return "VER-001 CECHL/CECHM/CECHP/CECHQxx";
+	// Slim
+	if(!strncmp(platform_id, "CokG11", PLATFORM_ID_MAX_LEN)) return "DYN-001 CECH-20xxA/B";
+	if(!strncmp(platform_id, "CokH11", PLATFORM_ID_MAX_LEN)) return "SUR-001 CECH-21xxA/B";
+	if(!strncmp(platform_id, "CokJ13", PLATFORM_ID_MAX_LEN)) return "JTP-001 CECH-25xxA/B";
+	if(!strncmp(platform_id, "CokJ20", PLATFORM_ID_MAX_LEN)) return "JSD-001 CECH-25xxA/B";
+	if(!strncmp(platform_id, "CokK10", PLATFORM_ID_MAX_LEN)) return "KTE-001 CECH-30xxA/B";
+	// Super Slim
+	if(!strncmp(platform_id, "CokM10", PLATFORM_ID_MAX_LEN)) return "MPX-001 NOR CECH-40xxB/C";
+	if(!strncmp(platform_id, "CokM20", PLATFORM_ID_MAX_LEN)) return "MSX-001 NOR CECH-40xxB/C";
+	if(!strncmp(platform_id, "CokM30", PLATFORM_ID_MAX_LEN)) return "MPX-001 eMMC CECH-40xxA";
+	if(!strncmp(platform_id, "CokN10", PLATFORM_ID_MAX_LEN)) return "NPX-001 NOR CECH-40xxB/C";
+	if(!strncmp(platform_id, "CokN30", PLATFORM_ID_MAX_LEN)) return "NPX-001 eMMC CECH-42xxA?"; // Wiki is unsure here
+	if(!strncmp(platform_id, "CokP10", PLATFORM_ID_MAX_LEN)) return "PQX-001 NOR CECH-42xxB/C";
+	if(!strncmp(platform_id, "CokP20", PLATFORM_ID_MAX_LEN)) return "PPX-001 NOR CECH-42xxB/C";
+	if(!strncmp(platform_id, "CokP30", PLATFORM_ID_MAX_LEN)) return "PQX-001 eMMC CECH-42xxA";
+	if(!strncmp(platform_id, "CokP40", PLATFORM_ID_MAX_LEN)) return "PPX-001 eMMC CECH-42xxA";
+	if(!strncmp(platform_id, "CokR10", PLATFORM_ID_MAX_LEN)) return "RTX-001 NOR CECH-43xxB/C";
+	if(!strncmp(platform_id, "CokR20", PLATFORM_ID_MAX_LEN)) return "REX-001 NOR CECH-43xxB/C";
+	if(!strncmp(platform_id, "CokR30", PLATFORM_ID_MAX_LEN)) return "RTX-001 eMMC CECH-43xxA";
+	if(!strncmp(platform_id, "CokR40", PLATFORM_ID_MAX_LEN)) return "REX-001 eMMC CECH-43xxA";
+	// Anything else
+	return "Unknown board, unknown model";
+}
 
-	if(print_time != NULL){
-		cellRtcSetTime_t(&cDate, print_time);
+static int format_date(char *buf, time_t time){
+	CellRtcDateTime cDate;
+	bool rtc_not_set = false;
+	int days = 0;
+
+	if(time != NULL){
+		cellRtcSetTime_t(&cDate, time);
+
+		// When RTC is reset, it starts counting from around 2005/12/31 00:00:00 (0x0B488680)
+		// Lets assume that all records from 2005 and 2006 are not actually that old
+		if(cDate.year == 2005 || cDate.year == 2006)
+		{
+			// Subtract the J2000 => 2005/12/31 offset and also the 1970=>J2000 offset
+			int64_t seconds_from_reset = time - 0x0B488680 - 946684800;
+
+			days = seconds_from_reset / (86400);
+			int remainder = seconds_from_reset % 86400;
+			cDate.hour = remainder / 3600;
+			remainder %= 3600;
+			cDate.minute = remainder / 60;
+			remainder %= 60;
+			cDate.second = remainder;
+
+			rtc_not_set = true;
+		}
 	} else {
 		cellRtcGetCurrentClockLocalTime(&cDate);
 	}
 
-	int written = sprintf(buf, "%04i-%02i-%02i %02i:%02i:%02i", cDate.year, cDate.month, cDate.day, cDate.hour, cDate.minute, cDate.second);
+	int written;
+	if(!rtc_not_set)
+	{
+		written = snprintf(buf, SYSCON_DATE_BUFF_LEN, "%04i-%02i-%02i %02i:%02i:%02i", cDate.year, cDate.month, cDate.day, cDate.hour, cDate.minute, cDate.second);
+	}
+	else
+	{
+		written = snprintf(buf, SYSCON_DATE_BUFF_LEN, "RTC: % 3id %02i:%02i:%02i", days, cDate.hour, cDate.minute, cDate.second);
+	}
 	
 	return (written > 0) ? written : 0;
 }
 
-void dump_syscon_errors(void){
-	char buf[SYSCON_ERROR_BUFF_LEN];
-	char datebuf[32];
-	bool need_to_writeback = false;
-	bool log_print_header = true;
-	bool initial_dump = false;
-	bool beeped = false;
+static int log_print(int fd_log, const char *str)
+{
+	char date[SYSCON_DATE_BUFF_LEN];
+	char buf[SYSCON_DATE_BUFF_LEN];
 
-	syscon_error error_cache[32];
-	memset(&error_cache, 0, sizeof(error_cache));
+	format_date(date, NULL);
+	snprintf(buf, SYSCON_DATE_BUFF_LEN, "[%s] ", date);
+	if(cellFsWrite(fd_log, buf, strlen(buf), NULL) != CELL_FS_SUCCEEDED) return false;
 
-	int fd_errors = -1;
-	int fd_log = -1;
+	if(cellFsWrite(fd_log, str, strlen(str), NULL) != CELL_FS_SUCCEEDED) return false;
+
+	return true;
+}
+
+static bool load_disk_cache(syscon_error *target, unsigned int size)
+{
+	int fd_cache = -1;
+	bool ret = true;
+
+	if(cellFsOpen(SYSCON_ERROR_CACHE, CELL_FS_O_RDONLY, &fd_cache, NULL, NULL) == CELL_FS_SUCCEEDED){
+		uint64_t nread = 0;
+
+		if(cellFsRead(fd_cache, target, size, &nread) == CELL_FS_SUCCEEDED){
+			if(nread != size){
+				memset(target, 0, size);
+				ret = false;
+			}
+		}
+
+		cellFsClose(fd_cache);
+	} else {
+		ret = false;
+	}
+
+	return ret;
+}
+
+static bool save_disk_cache(syscon_error *cache, unsigned int size)
+{
+	int fd_cache = -1;
+	bool ret = true;
+
+	if(cellFsOpen(SYSCON_ERROR_CACHE, CELL_FS_O_WRONLY|CELL_FS_O_CREAT|CELL_FS_O_TRUNC, &fd_cache, NULL, NULL) == CELL_FS_SUCCEEDED){
+		uint64_t nwrite = 0;
+
+		if(cellFsWrite(fd_cache, cache, size, &nwrite) == CELL_FS_SUCCEEDED){
+			if(nwrite == size){
+				// Write ok
+			} else {
+				ret = false;
+			}
+		} else {
+			ret = false;
+		}
+
+		cellFsClose(fd_cache);
+	}
+
+	return ret;
+}
+
+static bool load_syscon_errors(syscon_error *target)
+{
+	for(int i = 0; i < 32; i++){
+		uint8_t status;
+
+		sys_sm_request_error_log(i, &status, &target[i].error_code, &target[i].error_time);
+	}
+
+	return true;
+}
+
+static bool check_logfile(void)
+{
 	CellFsStat stat_log;
 	if(cellFsStat(SYSCON_ERROR_LOG, &stat_log) == CELL_FS_SUCCEEDED){
 		if(stat_log.st_size >= SYSCON_ERROR_LOG_MAXSIZE){
 			cellFsUnlink(SYSCON_ERROR_LOG);
 		} else {
-			log_print_header = false;
+			return true;
 		}
 	}
+	
+	return false;
+}
 
-	if(cellFsOpen(SYSCON_ERROR_LOG, CELL_FS_O_WRONLY|CELL_FS_O_CREAT|CELL_FS_O_APPEND, &fd_log, NULL, NULL) == CELL_FS_SUCCEEDED){
-		if(log_print_header){
-			initial_dump = true;
-			format_date(datebuf, NULL);
-			snprintf(buf, SYSCON_ERROR_BUFF_LEN, "[%s] Syscon error log, for more information, refer to https://www.psdevwiki.com/ps3/Syscon_Error_Codes\r\n", datebuf);
-			cellFsWrite(fd_log, buf, strlen(buf), NULL);
+static bool prepare_log_error(char *buf, syscon_error *cache, int index)
+{
+	// Original format is J2000, 1970-01-01 -> 2000-01-01, +30 years
+	time_t print_time = (time_t) ((uint32_t) cache[index].error_time + 946684800);
 
-			snprintf(buf, SYSCON_ERROR_BUFF_LEN, "[%s] --- Making initial dump ---\r\n", datebuf);
-			cellFsWrite(fd_log, buf, strlen(buf), NULL);
-		 }
+	const char *reason = getSysconErrorDesc(cache[index].error_code);
+	if(index == 31){
+		// The last record is always a loop mark
+		reason = "Loop mark";
 	}
 
-	if(cellFsOpen(SYSCON_ERROR_CACHE, CELL_FS_O_RDONLY, &fd_errors, NULL, NULL) == CELL_FS_SUCCEEDED){
-		uint64_t nread = 0;
+	char errdatebuf[SYSCON_DATE_BUFF_LEN];
+	format_date(errdatebuf, print_time);
+	snprintf(buf, SYSCON_ERROR_BUFF_LEN, "New error found: %08X (%s) at %s\r\n", cache[index].error_code, reason, errdatebuf);
+	return true;
+}
 
-		if(cellFsRead(fd_errors, &error_cache, sizeof(error_cache), &nread) == CELL_FS_SUCCEEDED){
-			if(nread != sizeof(error_cache)){
-				memset(&error_cache, 0, sizeof(error_cache));
-			}
-		}
+static bool print_log_header(int fd_log, syscon_error *cache)
+{
+	char buf[SYSCON_ERROR_BUFF_LEN];
 
-		cellFsClose(fd_errors);
-	} else {
-		need_to_writeback = true;
+	log_print(fd_log, "Syscon error log, for more information, refer to https://www.psdevwiki.com/ps3/Syscon_Error_Codes\r\n");
+
+	struct system_info hwinfo = {0, 0, 0, {0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, 0, {0, 0, 0, 0}};
+	int ret = sys_sm_get_system_info(&hwinfo);
+	if(!ret) {
+		snprintf(buf, SYSCON_ERROR_BUFF_LEN, "Platform ID: %s (%s)\r\n", hwinfo.platform_id, getBoardDesc(hwinfo.platform_id));
+		log_print(fd_log, buf);
 	}
 
-	for(int i = 0; i < 32; i++){
-		uint8_t status;
-		time_t print_time;
+	log_print(fd_log, "--- Making initial dump ---\r\n");
 
-		uint32_t orig_code = error_cache[i].error_code;
-		uint32_t orig_time = error_cache[i].error_time;
+	// Print errors backwards
+	for(int i = 31; i >= 0; i--){
+		prepare_log_error(buf, cache, i);
+		log_print(fd_log, buf);
+	}
+	
+	log_print(fd_log, "--- Initial dump complete ---\r\n");
 
-		uint32_t ret = sys_sm_request_error_log(i, &status, &error_cache[i].error_code, &error_cache[i].error_time);
-		if(!ret && !status){
-			// 1970-01-01 -> 2000-01-01, +30 years
-			print_time = (time_t) ((uint32_t) error_cache[i].error_time + 946684800);
+	return true;
+}
 
-			if(orig_code != error_cache[i].error_code || orig_time != error_cache[i].error_time){
-				format_date(datebuf, NULL);
+static bool print_new_errors(int fd_log, syscon_error *disk_cache, syscon_error *syscon_cache)
+{
+	bool beeped = false;
+	for(int x = 0; x < 32; x++)
+	{
+		// look for the first disk cache entry in syscon cache
+		// or for the last entry - loop mark
+		if(memcmp(&disk_cache[0], &syscon_cache[x], sizeof(syscon_error)) == 0 || x == 31) 
+		{
+			if(x > 0)
+			{
+				char buf[SYSCON_ERROR_BUFF_LEN];
 
-				const char *reason = getSysconErrorDesc(error_cache[i].error_code);
-
-				char errdatebuf[32];
-				format_date(errdatebuf, print_time);
-				snprintf(buf, SYSCON_ERROR_BUFF_LEN, "[%s] New error found %02d: %08X (%s) at %s\r\n", datebuf, i + 1, error_cache[i].error_code, reason, errdatebuf);
-				cellFsWrite(fd_log, buf, strlen(buf), NULL);
-
-				if(!initial_dump){
-					snprintf(buf, SYSCON_ERROR_BUFF_LEN, "Syscon error detected:\r\n%08X (%s)\r\n%s", error_cache[i].error_code, reason, errdatebuf);
+				for(int y = x-1; y >= 0; y--)
+				{
+					prepare_log_error(buf, syscon_cache, y);
+					log_print(fd_log, buf);
 
 					vshtask_notify(buf);
-					if(!beeped){
-						BEEP3; // Beep 3 times to alert the user
+					if(!beeped)
+					{
+						BEEP3;
 						beeped = true;
 					}
 				}
 
-				need_to_writeback = true;
+				// Errors found
+				return true;
 			}
-		} else {
-			break;
+
+			// No errors found
+			return false;
 		}
 	}
 
-	if(initial_dump){
-		format_date(datebuf, NULL);
-		snprintf(buf, SYSCON_ERROR_BUFF_LEN, "[%s] --- Initial dump complete ---\r\n", datebuf);
-		cellFsWrite(fd_log, buf, strlen(buf), NULL);
+	return false;
+}
+
+void dump_syscon_errors(void){
+	bool need_to_writeback = false;
+	bool print_header = true;
+
+	syscon_error disk_cache[32]; // Syscon error dump loaded from HDD
+	syscon_error syscon_cache[32]; // Syscon error dump from Syscon
+	memset(&disk_cache, 0, sizeof(disk_cache));
+	memset(&syscon_cache, 0, sizeof(syscon_cache));
+
+	int fd_log = -1;
+	
+	// Check if logfile exists, delete if it's too large
+	if(check_logfile()){
+		print_header = false;
 	}
 
-	if(need_to_writeback && cellFsOpen(SYSCON_ERROR_CACHE, CELL_FS_O_WRONLY|CELL_FS_O_CREAT|CELL_FS_O_TRUNC, &fd_errors, NULL, NULL) == CELL_FS_SUCCEEDED){
-		uint64_t nwrite = 0;
+	// Load all errors from Syscon
+	load_syscon_errors(syscon_cache);
 
-		if(cellFsWrite(fd_errors, &error_cache, sizeof(error_cache), &nwrite) == CELL_FS_SUCCEEDED){
-			if(nwrite == sizeof(error_cache)){
-				// Write ok
-			} else {
-				// Wrong length written
-			}
-		} else {
-			// Other error
-		}
-
-		cellFsClose(fd_errors);
+	// Load errors from HDD cache
+	if(!load_disk_cache(disk_cache, sizeof(disk_cache)))
+	{
+		need_to_writeback = true;
+		memcpy(disk_cache, syscon_cache, sizeof(disk_cache));
 	}
+
+	// Open logfile
+	if(cellFsOpen(SYSCON_ERROR_LOG, CELL_FS_O_WRONLY|CELL_FS_O_CREAT|CELL_FS_O_APPEND, &fd_log, NULL, NULL) != CELL_FS_SUCCEEDED)
+	{
+		vshtask_notify("PS3Mon: Cannot create syscon logfile!");
+		return;
+	}
+
+	// Print header and initial dump if necessary
+	if(print_header) print_log_header(fd_log, syscon_cache);
+
+	if(print_new_errors(fd_log, disk_cache, syscon_cache))
+	{
+		memcpy(disk_cache, syscon_cache, sizeof(disk_cache));
+		need_to_writeback = true;
+	}
+
+	if(need_to_writeback) save_disk_cache(disk_cache, sizeof(disk_cache));
 
 	cellFsClose(fd_log);
 }
